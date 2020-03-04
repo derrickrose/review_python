76ade6a542f48b1f87ad353985a5e2701b1214b7
AMPILAHY Frelin (Canal Plus Prestataire) <frelin.ampilahy@cpexterne.org>
2020-03-04 18:35:29 +0100

    review python

---
+++ multiplication/multiplication.py
@@ -0,0 +1,15 @@
+# -*-coding:ENCODAGE -*
+"""module multipli contenant la fonction table"""
+
+import os
+
+def table(nb,max=10):
+    i = 0
+    while i < max:
+        print(nb," * ",i+1," = ",nb*(i+1))
+        i+=1
+      
+# test de la fonction table
+if __name__ == "__main__":
+    table(4,12)
+    os.system("pause")
