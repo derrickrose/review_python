346382b6936c49cca08beae4d6a1ffd1a5fd26d7
AMPILAHY Frelin (Canal Plus Prestataire) <frelin.ampilahy@cpexterne.org>
2020-03-10 11:34:59 +0100

    add exceptions

---
+++ exception/exception.py
@@ -0,0 +1,17 @@
+from os import system
+#import os then use os.system()
+
+valor = int(input("donnez un nombre "))
+
+"""
+"""
+try: 
+    valor = 1 / valor
+except ZeroDivisionError as error:
+    print("error",error)
+"""
+"""
+
+
+system("pause")
+
